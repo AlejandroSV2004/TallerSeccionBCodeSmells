009f53a11a16aef083b5bbc0900588d479a4ca8b
AlejandroSV2004 <alesvera@espol.edu.ec>
2024-08-08 20:30:37 -0500

    Implementacion clase abstracta Opcion

---
+++ src/main/java/ec/edu/espol/codesmells/Opcion.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.codesmells;
+
+/**
+ *
+ * @author aleja
+ */
+public abstract class Opcion {
+    String descripcion;
+    String debilidad;
+}
---
+++ src/main/java/ec/edu/espol/codesmells/Papel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.codesmells;
+
+/**
+ *
+ * @author aleja
+ */
+public class Papel extends Opcion{
+    public Papel(){
+        this.descripcion = "Papel";
+        this.debilidad = "Tijera";
+    }
+}
