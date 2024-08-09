f6d7222188b690414b203b6eba20d108193aa81a
Vic <hvangulo@espol.edu.ec>
2024-08-08 20:32:26 -0500

    clase tijera

---
+++ src/main/java/ec/edu/espol/codesmells/Tijera.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.codesmells;
+
+/**
+ *
+ * @author Victoria
+ */
+public class Tijera extends Opcion {
+
+    public Tijera() {
+        this.descripcion = "Tijera";
+        this.debilidad = "Piedra";
+    }
+}
